2884165be51b66aaadca50847c76bd509e4454ae
Serzh <smazheluks@gmail.com>
2025-11-23 15:46:46 +0300

    feat: add telegram bot configuration options

---
+++ WordCoachBot/WordCoachBot.Application/Options/TelegramBotOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace WordCoachBot.Application.Options
+{
+    public class TelegramBotOptions
+    {
+        public const string SectionName = "TelegramBot";
+        public string Token { get; set; } = string.Empty;
+    }
+}
--- WordCoachBot/WordCoachBot.WebApi/Program.cs
+++ WordCoachBot/WordCoachBot.WebApi/Program.cs
@@ -1,5 +1,6 @@
-
+using Microsoft.Extensions.Options;
 using WordCoachBot.Application;
+using WordCoachBot.Application.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<TelegramBotOptions>(
+    builder.Configuration.GetSection(TelegramBotOptions.SectionName)
+);
 
 // Временная заглушка сервиса слов
 builder.Services.AddScoped<IWordService, DummyWordService>();
@@ -22,6 +26,18 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapGet("/api/ping", () => "WordCoachBot is up!");
+
+app.MapGet("/api/config-test", (IOptions<TelegramBotOptions> options) =>
+{
+    var token = options.Value.Token;
+    var hasToken = !string.IsNullOrWhiteSpace(token);
+
+    return Results.Ok(new
+    {
+        HasToken = hasToken,
+        TokenLength = token?.Length ?? 0
+    });
+});
 
 app.MapControllers();
 
